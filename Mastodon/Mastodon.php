bbe0e45284b0f00d20443f8d4947c45b739f3bcb
kalvn <kalvnthereal@gmail.com>
2018-01-11 20:17:56 +0100

    Removes a lot of unused code.

--- Mastodon/Mastodon.php
+++ Mastodon/Mastodon.php
@@ -31,27 +31,10 @@ class Mastodon
     ];
 
     /**
-     * Credentials to register App
-     * @var array
-     */
-    protected $credentials = [
-        'client_name'   => '',
-        'redirect_uris' => 'urn:ietf:wg:oauth:2.0:oob',
-        'scopes'        => 'read write follow',
-        'website'       => ''
-    ];
-
-    /**
      * Credentials to use Mastodon API
      * @var array
      */
     protected $appCredentials = [];
-
-    /**
-     * Authenticated User infos
-     * @var array
-     */
-    private $userInfos = [];
 
     /**
      * Setting Domain, like 'mastodon.social'
@@ -63,8 +46,6 @@ class Mastodon
 
         $this->http = new HttpRequest($this->domain);
 
-        $this->credentials['client_name'] = $parameters['client_name'];
-        $this->credentials['website'] = $parameters['website'];
         $this->appCredentials['bearer'] = $parameters['bearer'];
         $this->headers['Authorization'] = $parameters['bearer'];
     }
@@ -80,91 +61,6 @@ class Mastodon
     public function authentify($userName, $password)
     {
         return $this->authUser($userName, $password);
-    }
-
-    /**
-     * Get authenticated user
-     * 
-     * @return array
-     */
-    public function getUser()
-    {
-        if (empty($this->userInfos)) {
-            $user = $this->http->get(
-                $this->http->apiURL . 'accounts/verify_credentials',
-                $this->headers,
-                null
-            );
-            if (is_array($user) && isset($user['username'])) {
-                $this->userInfos = $user;
-            }
-            else {
-                return false;
-            }
-        }
-        return $this->userInfos;
-    }
-    
-    /**
-     * Get authenticated user ID
-     * 
-     * @return string
-     */
-    public function getUserID()
-    {
-        $user = $this->getUser();
-        if (!is_array($user)) {
-            return false;
-        }
-        return $user['id'];
-    }
-
-    /**
-     * Get authenticated user favourites
-     * 
-     * Return as an array all user's favourites status
-     * 
-     * @return array
-     */
-    public function getFavourites()
-    {
-        return $this->http->get(
-            $this->http->apiURL . 'favourites', 
-            $this->headers, 
-            null
-        );
-    }
-    
-    /**
-     * Get authenticated user's notifications
-     * Return as an array all user's notifications
-     * 
-     * @return array
-     */
-    public function getNotifications()
-    {
-        return $this->http->get(
-            $this->http->apiURL . 'notifications', 
-            $this->headers, 
-            null
-        );
-    }
-
-    /**
-     * Get an account by his ID
-     * 
-     * Return account as an array
-     * 
-     * @param string $id
-     * @return array
-     */
-    public function getAccount($id)
-    {
-        return $this->http->get(
-            $this->http->apiURL . 'accounts/' . $id, 
-            $this->headers,
-            null
-        );
     }
 
     /**
@@ -196,116 +92,6 @@ class Mastodon
                 'media_ids' => $medias
             ]
         );
-    }
-    
-    /**
-     * Get user's followers by his ID
-     * If not ID is given, return the authenticated user's followers
-     * 
-     * Return all followers as an array
-     * 
-     * @param string $id
-     * @param array  $params
-     * Params is optionnal associative array with these keys :
-     * limit : Maximum number of accounts to get (Default 40, Max 80)
-     * max_id : Get a list of followers with ID less than or equal this value
-     * since_id : Get a list of followers with ID greater than this value
-     * @return array
-     */
-    public function getFollowers($id = false, $params = [])
-    {
-        if ($id === false) {
-            $id = $this->getUserID();
-        }
-        return $this->http->get(
-            $this->http->apiURL . 'accounts/' . $id . '/followers',
-            $this->headers,
-            $params
-        );
-    }
-    
-    /**
-     * Get user's following by his ID
-     * If not ID is given, return the authenticated user's following
-     * 
-     * Return all followings as an array
-     * 
-     * @param string $id
-     * @return array
-     */
-    public function getFollowing($id = false)
-    {
-        if ($id === false) {
-            $id = $this->getUserID();
-        }
-        return $this->http->get(
-            $this->http->apiURL . 'accounts/' . $id . '/following',
-            $this->headers,
-            null
-        );
-    }
-    
-    /**
-     * Get user's statuses by his ID
-     * If not ID is given, return the authenticated user's statuses
-     * 
-     * Return all statuses as an array
-     * 
-     * @param string $id
-     * @param array  $params
-     * Params is optionnal associative array with these keys :
-     * only_media : Only return statuses that have media attachments
-     * exclude_replies : Skip statuses that reply to other statuses
-     * @return array
-     */
-    public function getStatuses($id = false, $params = [])
-    {
-        if ($id === false) {
-            $id = $this->getUserID();
-        }
-        return $this->http->get(
-            $this->http->apiURL . 'accounts/' . $id . '/statuses',
-            $this->headers,
-            $params
-        );
-    }
-
-    /**
-     * Upload a media from drive to use it in a toot
-     * 
-     * @param string $filePath Path to the media
-     * @return array
-     */
-    public function createAttachement($filePath)
-    {        
-        $mimeType = mime_content_type($filePath);
-        $curlFile = curl_file_create( $filePath, $mimeType, 'file' );
- 
-        $url = $this->http->domainURL . $this->http->apiURL . 'media';
-        $headers = [
-            'Content-Type' => 'multipart/form-data',
-        ];
- 
-        $postField =[
-            'access_token'  => $this->appCredentials['bearer'],
-            'file'          => $curlFile,
-        ];
- 
-        $opts = [
-            CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_HTTPHEADER     => $headers,
-            CURLOPT_URL            => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_POST           => true,
-            CURLOPT_POSTFIELDS     => $postField,
-        ];
- 
-        $curlHandle = curl_init();
-        curl_setopt_array( $curlHandle, $opts );
-        $response = curl_exec( $curlHandle );
-        curl_close( $curlHandle );
- 
-        return json_decode( $response, true );
     }
     
     /**
@@ -356,28 +142,9 @@ class Mastodon
             // Add Token to AppCredentials and save it
             $this->appCredentials['bearer'] = $token['access_token'];
             $this->headers['Authorization'] = 'Bearer ' . $token['access_token'];
-            $this->saveCredentialsToJSON($this->appCredentials);
+            //$this->saveCredentialsToJSON($this->appCredentials);
             return $token['access_token'];
         }
         return false;
-    }
-
-    /**
-     * Create new App
-     * 
-     * Create new application with credentials given in 'registerApp' method
-     * and save IDs in the JSON file to use them later.
-     * 
-     * Return true if success
-     * 
-     * @return boolean
-     */
-    protected function createApp()
-    {
-        $credentials = $this->http->post($this->http->apiURL . 'apps', $this->headers, $this->credentials);
-        if ($credentials === false) {
-            return false;
-        }
-        return $this->saveCredentialsToJSON($credentials);
     }
 }
